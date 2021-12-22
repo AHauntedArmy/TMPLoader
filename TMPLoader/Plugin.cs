feb14f4afc6c92bb627cfed2b1ee52c8bd01d2b6
AHauntedArmy <extreme15@hotmail.co.uk>
2021-12-22 20:10:24 +0000

    Add project files.

---
+++ TMPLoader/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+
+namespace TMPLoader
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class TMPLoader : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            
+        }
+    }
+}
