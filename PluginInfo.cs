07ca10c722726ca64d9b1aec80b2fa1cee740751
BzzzThe18th <69125495+BzzzThe18th@users.noreply.github.com>
2023-05-26 21:43:38 -0700

    update for beach

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TMPLoader
+{
+    class PluginInfo
+    {
+        public const string GUID = "com.ahauntedarmy.gorillatag.tmploader";
+        public const string Name = "TMPLoader";
+        public const string Version = "1.0.2";
+    }
+}
